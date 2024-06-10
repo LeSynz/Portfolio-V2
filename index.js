730fd5171e56638d0e17f6d2ed18b5d3d1cb10db
LeSynz <dbrennan@swaay.co.uk>
2024-06-10 20:27:05 +0100

    added a head.ejs and some more

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
+app.set('views', '/views');
+app.use(express.static('/public'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
