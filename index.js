07b90574c540604df6344479d38dec2ee6a95a83
LeSynz <dbrennan@swaay.co.uk>
2024-06-10 20:40:39 +0100

    Some templating

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.set('view engine', 'ejs');
 app.set('views', '/views');
 app.use(express.static('/public'))
 
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
